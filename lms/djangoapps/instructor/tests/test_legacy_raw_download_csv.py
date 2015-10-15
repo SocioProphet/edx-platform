cb247e8fdebb7108db4c260e03493d42086dba8d
Shawn Milochik <shawn@milochik.com>
2015-10-15 16:59:24 -0400

    Adds more tests for get_student_grade_summary_data.
    Tests all the kwargs. This is in support of #95, and will be followed
    by an update to get_student_grade_summary_data which adds a new kwarg.

--- lms/djangoapps/instructor/tests/test_legacy_raw_download_csv.py
+++ lms/djangoapps/instructor/tests/test_legacy_raw_download_csv.py
@@ -67,14 +67,17 @@ class TestRawGradeCSV(TestSubmittingProblems):
 '''
         self.assertEqual(body, expected_csv, msg)
 
-    def test_grade_summary_data(self):
+    def get_expected_data(
+        self, get_grades=True, get_raw_scores=False, use_offline=False,
+    ):
         """
-        Test grade summary data report generation
-        """
-        self.answer_question()
+        Return expected results from the get_student_grade_summary_data call
+        with any options selected.
 
-        request = DummyRequest()
-        data = get_student_grade_summary_data(request, self.course, get_raw_scores=False)
+        Note that the kwargs accepted by get_expected_data (and their
+        default values) must be identical to those in
+        get_student_grade_summary_data.
+        """
         expected_data = {
             'students': [self.student_user, self.student_user2],
             'header': [
@@ -103,6 +106,147 @@ class TestRawGradeCSV(TestSubmittingProblems):
                 u'Lab 10', u'Lab 11', u'Lab 12', u'Lab Avg', u'Midterm', u'Final'
             ]
         }
+
+        non_grade_columns = 5
+        if (get_grades is False) or (use_offline is True):
+            expected_data["header"] = expected_data["header"][:non_grade_columns]
+            for index, rec in enumerate(expected_data["data"]):
+                expected_data["data"][index] = rec[:non_grade_columns]
+            if use_offline is True:
+                expected_data['assignments'] = []
+            if get_grades is False:
+                del expected_data['assignments']
+        elif get_raw_scores is True:
+            expected_data["data"] =  [
+                [
+                    1, u'u1', u'username', u'view@test.com',
+                    '', None, None, None
+                ],
+                [
+                    2, u'u2', u'username', u'view2@test.com',
+                    '', 0.0, 1.0, 0.0
+                ],
+            ]
+            expected_data["assignments"] = [u'p3', u'p2', u'p1']
+            expected_data["header"] = [
+                u'ID', u'Username', u'Full Name', u'edX email',
+                u'External email', u'p3', u'p2', u'p1'
+            ]
+
+        return expected_data
+
+    def test_grade_summary_data_defaults(self):
+        """
+        Test grade summary data report generation with all default kwargs.
+        """
+        request = DummyRequest()
+        self.answer_question()
+        data = get_student_grade_summary_data(request, self.course)
+        expected_data = self.get_expected_data()
+
+        for key in ['assignments', 'header']:
+            self.assertListEqual(expected_data[key], data[key])
+
+        for index, student in enumerate(expected_data['students']):
+            self.assertEqual(
+                student.username,
+                data['students'][index].username
+            )
+            self.assertListEqual(
+                expected_data['data'][index],
+                data['data'][index]
+            )
+
+    def test_grade_summary_data_raw_scores(self):
+        """
+        Test grade summary data report generation with get_raw_scores True.
+        """
+        request = DummyRequest()
+        self.answer_question()
+        data = get_student_grade_summary_data(
+            request, self.course, get_raw_scores=True)
+        expected_data = self.get_expected_data(get_raw_scores=True)
+
+        for key in ['assignments', 'header']:
+            self.assertListEqual(expected_data[key], data[key])
+
+        for index, student in enumerate(expected_data['students']):
+            self.assertEqual(
+                student.username,
+                data['students'][index].username
+            )
+            self.assertListEqual(
+                expected_data['data'][index],
+                data['data'][index]
+            )
+
+    def test_grade_summary_data_no_grades(self):
+        """
+        Test grade summary data report generation with
+        get_grades set to False.
+        """
+        request = DummyRequest()
+        self.answer_question()
+        data_sets = []
+
+        # vanilla
+        data_sets.append(get_student_grade_summary_data(
+            request, self.course, get_grades=False))
+        # get_raw_scores and use_offline do nothing if
+        # get_grades is False.
+        data_sets.append(get_student_grade_summary_data(
+            request, self.course, get_grades=False, get_raw_scores=False))
+        data_sets.append(get_student_grade_summary_data(
+            request, self.course, get_grades=False, use_offline=False))
+
+        expected_data = self.get_expected_data(get_grades=False)
+        self.assertFalse("assignments" in expected_data)
+
+        for data in data_sets:
+            for index, student in enumerate(expected_data['students']):
+                self.assertEqual(
+                    student.username,
+                    data['students'][index].username
+                )
+                self.assertListEqual(
+                    expected_data['data'][index],
+                    data['data'][index]
+                )
+
+    def test_grade_summary_data_use_offline(self):
+        """
+        Test grade summary data report generation with use_offline True.
+        """
+        request = DummyRequest()
+        self.answer_question()
+        data = get_student_grade_summary_data(
+            request, self.course, use_offline=True)
+        expected_data = self.get_expected_data(use_offline=True)
+
+        for key in ['assignments', 'header']:
+            self.assertListEqual(expected_data[key], data[key])
+
+        for index, student in enumerate(expected_data['students']):
+            self.assertEqual(
+                student.username,
+                data['students'][index].username
+            )
+            self.assertListEqual(
+                expected_data['data'][index],
+                data['data'][index]
+            )
+
+    def test_grade_summary_data_use_offline_and_raw_scores(self):
+        """
+        Test grade summary data report generation with use_offline
+        and get_raw_scores both True.
+        """
+        request = DummyRequest()
+        self.answer_question()
+        data = get_student_grade_summary_data(
+            request, self.course, use_offline=True, get_raw_scores=True)
+        expected_data = self.get_expected_data(
+            use_offline=True, get_raw_scores=True)
 
         for key in ['assignments', 'header']:
             self.assertListEqual(expected_data[key], data[key])
